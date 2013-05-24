34e92abd9282e14d2054d2cba015165c7b6a88b7
alexandre <alexandre.artimos@gmail.com>
2013-05-23 22:26:23 -0300

    Primeiro commit de casa

--- Estudo/src/Teste.java
+++ Estudo/src/Teste.java
@@ -1,6 +1,11 @@
 public class Teste {
-	
-	public static void main(String[] args) {
-		System.out.println("Teste");
+
+	public String primeiroTeste() {
+		return "teste";
 	}
+
+	public static void main(String[] args) {
+		System.out.println(new Teste().primeiroTeste());
+	}
+
 }
